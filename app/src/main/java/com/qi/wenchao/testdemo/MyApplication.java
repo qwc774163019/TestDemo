d1dd8341c6d5d680b21234342d83dd4dc00990da
qiwenchao <qiwenchao@yongche.com>
2016-06-23 17:44:00 +0800

    Initial commit

---
+++ app/src/main/java/com/qi/wenchao/testdemo/MyApplication.java
@@ -0,0 +1,16 @@
+package com.qi.wenchao.testdemo;
+
+import android.app.Application;
+
+/**
+ * @author chao
+ * @Description {todo}
+ * @date 16/6/23 下午5:36
+ * @version: Version 7.0
+ */
+public class MyApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

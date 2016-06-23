6ff406bb31600056233a086e8b40e2f710248a18
qiwenchao <qiwenchao@yongche.com>
2016-06-23 20:11:46 +0800

    update

--- app/src/main/java/com/qi/wenchao/testdemo/MyApplication.java
+++ app/src/main/java/com/qi/wenchao/testdemo/MyApplication.java
@@ -2,11 +2,13 @@ package com.qi.wenchao.testdemo;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * @author chao
@@ -18,8 +20,14 @@ public class MyApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        initImageLoader(this);
+        LeakCanary.install(this);//添加内存泄漏检测工具
     }
 
+    /**
+     * 添加ImageLoader
+     * @param context
+     */
     public static void initImageLoader(Context context) {
         // This configuration tuning is custom. You can tune every option, you may tune some of them,
         // or you can create default configuration by
@@ -34,5 +42,11 @@ public class MyApplication extends Application{
                 .build();
         // Initialize ImageLoader with configuration.
         ImageLoader.getInstance().init(config);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
